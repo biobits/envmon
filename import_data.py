513a4aeaa673f7ab245172b2f5d5492725fc652e
Stefan Bartels <biobits@users.noreply.github.com>
2024-04-12 22:58:57 +0200

    update import_data

--- import_data.py
+++ import_data.py
@@ -11,15 +11,18 @@ import tempmon_dal as tmd
 dbpath = 'KlimaPi.sqlite'
 
 file_to_import='klimadatalogTest.log'
-
+file_to_import2='C:\Data\klimalog.log'
 #DB Connection
 con = sqlite3.connect(dbpath)
 
 # Read CSV CLimatedata
 headers=['timestamp','sensorid','temp','hum']
 
-kdat=pd.read_csv(file_to_import,sep='\t',header=None,  names=headers)#,parse_dates=[0])
-kdat=kdat.sort_values(by=['timestamp'])
+kdat=pd.read_csv(file_to_import,sep='\t',header=None,  names=headers).dropna()#,parse_dates=[0])
+kdat2=pd.read_csv(file_to_import2,sep='\t',header=None,  names=headers,skiprows=1000).dropna(),#,parse_dates=[0])
+alldat=kdat._append(kdat2,ignore_index=True)
+alldat=alldat.sort_values(by=['timestamp'])
+# Hier die Daten aus dem Jahr 1970 filtern  .filter(pd.to_datetime('Timestamp')>pd.to_datetime('2000-01-01 00:00:00'))
 
 # Read existing data in DB
 dbdat=pd.read_sql_query("SELECT * FROM measurements",con)
